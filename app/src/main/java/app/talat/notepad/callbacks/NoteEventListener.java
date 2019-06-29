4f31009f5c77e590d1947850b587a4c690997db0
TlTBerkay <berkayyildirim871@gmail.com>
2019-06-29 23:39:12 +0300

    Initial commit

---
+++ app/src/main/java/app/talat/notepad/callbacks/NoteEventListener.java
@@ -0,0 +1,20 @@
+package app.talat.notepad.callbacks;
+
+import app.talat.notepad.model.Note;
+
+
+public interface NoteEventListener {
+    /**
+     * call wen note clicked.
+     *
+     * @param note: note item
+     */
+    void onNoteClick(Note note);
+
+    /**
+     * call wen long Click to note.
+     *
+     * @param note : item
+     */
+    void onNoteLongClick(Note note);
+}
---
+++ app/src/main/java/app/talat/notepad/db/NotesDB.java
@@ -0,0 +1,26 @@
+package app.talat.notepad.db;
+
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+import app.talat.notepad.model.Note;
+
+
+@Database(entities = Note.class, version = 1)
+public abstract class NotesDB extends RoomDatabase {
+    public abstract NotesDao notesDao();
+
+    public static final String DATABSE_NAME = "notesDb";
+    private static NotesDB instance;
+
+    public static NotesDB getInstance(Context context) {
+        if (instance == null)
+            instance = Room.databaseBuilder(context, NotesDB.class, DATABSE_NAME)
+                    .allowMainThreadQueries()
+                    .build();
+        return instance;
+    }
+}
